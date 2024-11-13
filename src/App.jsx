930d931bef24163166a687765dd82b4ad9b435ed
Onixdrew <andrewmosquera1811@gmail.com>
2024-11-13 16:32:29 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Login from "./pages/login.jsx";
+function App() {
+  return (
+    <>
+      <header>
+        <div>
+          <h1>Holaa</h1>
+        </div>
+      </header>
+      <main>
+        <Login />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/login.jsx
@@ -0,0 +1,18 @@
+export function Login() {
+  return (
+    <>
+      <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+        <div className="bg-cyan-100 flex rounded-2xl shadow-lg max-w-3x1 p-5">
+
+          <div className="w-1/2">
+            <h2>test</h2>
+          </div>
+
+          <div className="w-1/2 p-5">
+            <img className="rounded-2xl" src="https://img.freepik.com/foto-gratis/adorable-gatito-pared-monocromatica-detras-ella_23-2148955142.jpg?t=st=1731529571~exp=1731533171~hmac=12479f78858df064a776322ad980cb526a02955a3b1d7c7a0e995570ef33d720&w=740"></img>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
